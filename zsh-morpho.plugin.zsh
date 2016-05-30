61e8422b9d5bdc17608f468a65b41580c8587238
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-30 11:55:42 +0200

    Inform about compatibility with cmatrix

--- zsh-morpho.plugin.zsh
+++ zsh-morpho.plugin.zsh
@@ -5,7 +5,8 @@
 # to ~/.zshrc.
 #
 # Available zstyles. Given values are the defaults
-# zstyle ":morpho" screen-saver "morpho" # select screen-saver "morpho"
+# zstyle ":morpho" screen-saver "morpho" # select screen-saver "morpho"; this  can also be
+#                                        # a command, e.g. "cmatrix"
 # zstyle ":morpho" delay "310"           # 5 minutes before screen-saver starts
 # zstyle ":morpho" check-interval "60"   # check every 1 minute if to run screen-saver
 # zstyle ":morpho" redraw-delay   "300"  # how often screen-saver redraws
