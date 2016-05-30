2fbbed1593b75a326dd3f508f3737b6984e2f986
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-30 11:39:20 +0200

    Standard screen saver framework should be now complete

--- zsh-morpho.plugin.zsh
+++ zsh-morpho.plugin.zsh
@@ -4,6 +4,11 @@
 #
 # to ~/.zshrc.
 #
+# Available zstyles. Given values are the defaults
+# zstyle ":morpho" screen-saver "morpho" # select screen-saver "morpho"
+# zstyle ":morpho" delay "310"           # 5 minutes before screen-saver starts
+# zstyle ":morpho" check-interval "60"   # check every 1 minute if to run screen-saver
+# zstyle ":morpho" redraw-delay   "300"  # how often screen-saver redraws
 
 REPO_DIR="${0%/*}"
 
@@ -17,6 +22,52 @@ if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$REPO_DIR]}" != $REPO_DIR ]]; then
     fpath+=( "$REPO_DIR" )
 fi
 
-autoload morlib morpho
-zle -N morpho
-bindkey '^t' morpho
+# Using sched module
+zmodload zsh/sched
+
+# Autoloads
+autoload morlib morpho add-zsh-hook
+
+# Global variables
+typeset -g morpho_check_interval
+zstyle -s ':morpho' check-interval morpho_check_interval || morpho_check_interval=60
+
+# Marks a shell that runs a command (i.e. displays a prompt)
+# as active, so that the screen saver will be run only there
+-morpho-active() {
+    echo "$$\\n$(date +%s)" > /tmp/morpho-active-shell
+}
+
+# Called periodically
+-morpho-check-run-screen-saver() {
+    typeset -a shell_data
+    shell_data=( "${(@f)"$(</tmp/morpho-active-shell)"}" )
+
+    # Only run screen-saver in last active shell
+    # (in shell where prompt has been redrawn last)
+    if [ "${shell_data[1]}" != "$$" ]; then
+        sched +$morpho_check_interval -morpho-check-run-screen-saver
+        return
+    fi
+
+    integer active_timestamp=${shell_data[2]}
+    integer current_timestamp=$(date +%s)
+
+    # Load expected delay
+    integer delay
+    zstyle -s ':morpho' delay delay || delay=310
+
+    if (( current_timestamp - active_timestamp > delay )); then
+        # Load wanted screen saver
+        local screen_saver
+        zstyle -s ':morpho' screen-saver screen_saver || screen_saver="morpho"
+        "$screen_saver"
+    fi
+
+    sched +$morpho_check_interval -morpho-check-run-screen-saver
+}
+
+sched +$morpho_check_interval -morpho-check-run-screen-saver
+
+# Redraw of prompt will trigger marking shell as active
+add-zsh-hook precmd -morpho-active
