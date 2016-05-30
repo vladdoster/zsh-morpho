156be8f7c663cdfc7638f83c66ed4c144ebe3520
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-30 21:09:14 +0200

    Set default delay to be slightly less than 5 minutes

--- zsh-morpho.plugin.zsh
+++ zsh-morpho.plugin.zsh
@@ -7,7 +7,7 @@
 # Available zstyles. Given values are the defaults
 # zstyle ":morpho" screen-saver "zmorpho" # select screen-saver "zmorpho"; available: zmorpho, zmandelbrot, zblank, pmorpho
 #                                         # this  can also be a command, e.g. "cmatrix"
-# zstyle ":morpho" delay "310"            # 5 minutes before screen-saver starts
+# zstyle ":morpho" delay "290"            # 5 minutes before screen-saver starts
 # zstyle ":morpho" check-interval "60"    # check every 1 minute if to run screen-saver
 # zstyle ":morpho" redraw-delay "300"     # how often screen-saver redraws
 
@@ -59,7 +59,7 @@ zstyle -s ':morpho' check-interval morpho_check_interval || morpho_check_interva
 
     # Load expected delay
     integer delay
-    zstyle -s ':morpho' delay delay || delay=310
+    zstyle -s ':morpho' delay delay || delay=290
 
     if (( current_timestamp - active_timestamp > delay )); then
         # Load wanted screen saver
