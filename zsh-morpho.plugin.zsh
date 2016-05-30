dbe648d7832ba361bc2d0cb4d5a17033e25a1737
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-30 07:45:31 +0200

    Make the plugin standalone

--- zsh-morpho.plugin.zsh
+++ zsh-morpho.plugin.zsh
@@ -1,3 +1,22 @@
+#
+# No plugin manager is needed to use this file. All that is needed is adding:
+#   source {where-zsh-morpho-is}/zsh-morpho.plugin.zsh
+#
+# to ~/.zshrc.
+#
+
+REPO_DIR="${0%/*}"
+
+#
+# Update FPATH if:
+# 1. Not loading with Zplugin
+# 2. Not having fpath already updated (that would equal: using other plugin manager)
+#
+
+if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$REPO_DIR]}" != $REPO_DIR ]]; then
+    fpath+=( "$REPO_DIR" )
+fi
+
 autoload morpho
 zle -N morpho
 bindkey '^t' morpho
