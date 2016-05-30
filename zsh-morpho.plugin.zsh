f0d912d96143b9e0c262ec6ef6bb5683ef4954de
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-30 12:27:42 +0200

    Add mandelbrot screen saver

--- zsh-morpho.plugin.zsh
+++ zsh-morpho.plugin.zsh
@@ -5,8 +5,8 @@
 # to ~/.zshrc.
 #
 # Available zstyles. Given values are the defaults
-# zstyle ":morpho" screen-saver "morpho" # select screen-saver "morpho"; this  can also be
-#                                        # a command, e.g. "cmatrix"
+# zstyle ":morpho" screen-saver "morpho" # select screen-saver "morpho"; available: morpho, mandelbrot
+#                                        # this  can also be a command, e.g. "cmatrix"
 # zstyle ":morpho" delay "310"           # 5 minutes before screen-saver starts
 # zstyle ":morpho" check-interval "60"   # check every 1 minute if to run screen-saver
 # zstyle ":morpho" redraw-delay   "300"  # how often screen-saver redraws
@@ -27,7 +27,7 @@ fi
 zmodload zsh/sched
 
 # Autoloads
-autoload morlib morpho add-zsh-hook
+autoload morlib morpho mandelbrot add-zsh-hook
 
 # Global variables
 typeset -g morpho_check_interval
